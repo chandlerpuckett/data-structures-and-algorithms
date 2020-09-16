241b65b485c635e5249e1498584f9251cf9667e4
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-16 14:42:01 -0700

    problem solved

--- challenges/src/main/java/challenges/BinarySearch.java
+++ challenges/src/main/java/challenges/BinarySearch.java
@@ -1,4 +1,30 @@
 package challenges;
 
+
+
 public class BinarySearch {
+
+    public int binarySearch (int[] arr, int searchKey){
+
+        int index = -1;
+        int low = 0;
+        int high = arr.length;
+
+        while (low <= high){
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < searchKey){
+                low = mid + 1;
+            } else if (arr[mid] > searchKey){
+                high = mid - 1;
+            } else if (arr[mid] == searchKey){
+                index = mid;
+                break;
+            }
+        }
+
+        return index;
+    }
+
 }
