cb2849e6569feafaae1ec6a627c7218424a69893
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-27 17:59:38 -0700

    test start

---
+++ DataStructures/src/test/java/DataStructures/QueueTest.java
@@ -0,0 +1,15 @@
+package DataStructures;
+
+/*
+Tests:
+    1. successfully enqueue
+    2. enqueue multiple values
+    3. dequeue an expected value
+    4. peek expected value
+    5. empty queue w/ multiple deQueues
+    6. instantiate empty queue
+    7. peek & dequeue throws exception on empty queue
+ */
+
+public class QueueTest {
+}
---
+++ DataStructures/src/test/java/DataStructures/StackTest.java
@@ -0,0 +1,16 @@
+package DataStructures;
+
+/*
+Tests:
+    1. push onto a stack
+    2. push multiple values
+    3. pop off the stack
+    4. empty stack after multiple pops
+    5. peek next item on stack
+    6. instantiate empty stack
+    7. empty stack throws exception
+ */
+
+public class StackTest {
+
+}
