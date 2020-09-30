2dedc5025e259276ebf83a4edaf008253e9c5277
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-30 14:07:45 -0700

    algorithm working

--- challenges/src/main/java/challenges/utilities/App.java
+++ challenges/src/main/java/challenges/utilities/App.java
@@ -12,6 +12,16 @@ public class App {
         shelter.enQ(wilbur);        // dog 2
         shelter.enQ(garfield);      // dog 3
 
-        System.out.println(shelter);
+//        System.out.println(shelter);
+
+//        ----- Multi Bracket Validation -----
+        String test1 = "{}{Code}[Fellows](())";     // T
+        String test2 = "[({}]";                     // F
+        String test3 = "()[[Extra Characters]]";    // T
+
+        System.out.println(MultiBracketValidation.isValid(test1));
+        System.out.println(MultiBracketValidation.isValid(test2));
+        System.out.println(MultiBracketValidation.isValid(test3));
+
     }
 }
