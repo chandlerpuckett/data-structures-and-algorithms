f5de8bc56bcc7b6599abc24fb63fba9fd1971b29
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-30 14:21:50 -0700

    tests

--- challenges/src/test/java/challenges/BracketTest.java
+++ challenges/src/test/java/challenges/BracketTest.java
@@ -11,9 +11,13 @@ public class BracketTest {
         String test1 = "{}{Code}[Fellows](())";     // T
         String test2 = "[({}]";                     // F
         String test3 = "()[[Extra Characters]]";    // T
+        String test4 = "{}[]()(PARTY TIME)";        // T
+        String test5 = "{{{{{((())]]{}{}";          // F
 
         assertEquals("this string is true", true, MultiBracketValidation.isValid(test1));
         assertEquals("this string is false", false, MultiBracketValidation.isValid(test2));
         assertEquals("this string is true", true, MultiBracketValidation.isValid(test3));
+        assertEquals("this string is true", true, MultiBracketValidation.isValid(test4));
+        assertEquals("this string is false", false, MultiBracketValidation.isValid(test5));
     }
 }
