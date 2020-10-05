093c81bbc9dff62aba14271eb4e2fd4f8b752247
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-04 23:29:06 -0700

    binary search tree functional, add & contains methods working

--- DataStructures/src/main/java/DataStructures/tree/App.java
+++ DataStructures/src/main/java/DataStructures/tree/App.java
@@ -5,9 +5,26 @@ public class App {
 
         BinaryTree newTree = new BinaryTree().createTree();
 
+        System.out.println("Binary Tree -----");
         System.out.println("Pre Order: " + newTree.preOrder(newTree.root));
         System.out.println("In Order: " + newTree.inOrder(newTree.root));
         System.out.println("Post Order: " + newTree.postOrder(newTree.root));
+        System.out.println();
+
+        BinarySearchTree bstTree = new BinarySearchTree();
+        bstTree.add(5);
+        bstTree.add(10);
+        bstTree.add(14);
+        bstTree.add(2);
+        bstTree.add(8);
+
+        System.out.println("Binary Search Tree -----");
+        System.out.println("Pre Order: " + bstTree.preOrder(bstTree.root));
+        System.out.println("In Order: " + bstTree.inOrder(bstTree.root));
+        System.out.println("Post Order: " + bstTree.postOrder(bstTree.root));
+        System.out.println("Contains? (8) " + bstTree.contains(bstTree.root, 8));
+        System.out.println("Contains? (21) " + bstTree.contains(bstTree.root, 21));
+
 
 
     }
--- DataStructures/src/main/java/DataStructures/tree/BinarySearchTree.java
+++ DataStructures/src/main/java/DataStructures/tree/BinarySearchTree.java
@@ -1,18 +1,41 @@
 package DataStructures.tree;
 
 public class BinarySearchTree extends BinaryTree {
+    Node root;
 
     public BinarySearchTree(){}
 
-    public boolean contains(Node root, int value){
-        if (root.value == value)
+//    ----- add to tree -----
+    public void add (int value){
+        root = addRecursive(root,value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+
+        // if current node is empty, return new Node(value)
+        if (current == null){return new Node(value);}
+
+        if (value < current.value){
+            current.left = addRecursive(current.left,value);
+        } else if (value > current.value){
+            current.right = addRecursive(current.right, value);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+//    ----- check tree for value -----
+    public boolean contains(Node current, int value){
+        if (current == null)
+            return false;
+
+        if (value == current.value)
             return true;
 
-        if (root.value > value) {
-            return contains(root.left, value);
-        } else {
-            return contains(root.right, value);
-        }
+        return value < current.value
+                ? contains(current.left, value)
+                : contains(current.right,value);
 
     }
 }
