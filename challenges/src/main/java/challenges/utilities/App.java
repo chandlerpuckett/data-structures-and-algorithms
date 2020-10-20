e98f20cd7e65c167336f0cba6979009f3f98e26c
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-20 14:56:34 -0700

    working

--- challenges/src/main/java/challenges/utilities/App.java
+++ challenges/src/main/java/challenges/utilities/App.java
@@ -8,5 +8,16 @@ public class App {
         for (int num : arr)
             System.out.print(num + " ");
 
+
+//       ---- merge sort ----
+
+        int[] arr2 = {12,11,13,5,6,7};
+        MergeSort.sort(arr2,0, arr2.length-1);
+
+        System.out.println("----- merger sorted array -----");
+        for (int num : arr2){
+            System.out.println(num + " ");
+        }
+
     }
 }
