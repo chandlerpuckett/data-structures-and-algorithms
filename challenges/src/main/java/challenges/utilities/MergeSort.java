c48f8990ab424707c41c9b9e1db42a51b3addac9
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-20 15:06:47 -0700

    code working

--- challenges/src/main/java/challenges/utilities/MergeSort.java
+++ challenges/src/main/java/challenges/utilities/MergeSort.java
@@ -1,53 +1,58 @@
 package challenges.utilities;
 
-public class MergeSort {
-    public static void merge(int[] arr, int left, int mid, int right){
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+class MergeSort {
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
         int L[] = new int[n1];
         int R[] = new int[n2];
 
-        for (int i=0; i < n1; i++)
-            L[i] = arr[left + i];
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-        for (int j=0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
 
         int i = 0, j = 0;
-        int k = 1;
-        while (i < n1 && j < n2){
-            if (L[i] <= R[j]){
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 i++;
-            } else {
+            }
+            else {
                 arr[k] = R[j];
                 j++;
             }
             k++;
         }
 
-        while (i < n1){
+        while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
-        while (j < n2){
+        while (j < n2) {
             arr[k] = R[j];
             j++;
             k++;
         }
     }
 
-    public static void sort(int arr[], int left, int right){
-        if (left > right){
-            int mid = (left + right) / 2;
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            int m = (l + r) / 2;
 
-            sort(arr, left, mid);
-            sort(arr, mid + 1, right);
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
 
-            merge(arr,left,mid,right);
+            merge(arr, l, m, r);
         }
     }
+
 }
