9230e24735573a0f5343881294380d7abd09526c
Chandler Puckett <chandler.puckett1@gmail.com>
2020-11-04 14:51:16 -0800

    code commented out

--- challenge401/src/main/java/datastructures/depthfirst/DepthFirst.java
+++ challenge401/src/main/java/datastructures/depthfirst/DepthFirst.java
@@ -1,31 +1,36 @@
 package datastructures.depthfirst;
 
-import datastructures.graph.Graph;
-import datastructures.stacksandqueues.Stack;
-import org.apache.commons.math3.geometry.spherical.twod.Vertex;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import org.apache.commons.math3.geometry.spherical.twod.Edge;
+import org.w3c.dom.Node;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class DepthFirst {
 //    https://www.baeldung.com/java-depth-first-search
 
     // -------- NOT WORKING YET
-
-//    public Set<String> depthFirst (Graph g1, String root){
-//        Set<String> visited = new LinkedHashSet<String>();
-//        Stack<String> stack = new Stack<String>();
-//        stack.push(root);
+//    public static LinkedList<Node> depthFirstSearch(Node start) {
+//        LinkedList result = new LinkedList();
+//        HashSet<Node> visited = new HashSet<>();
+//
+//        Stack stack = new Stack();
+//        stack.push(start);
+//        visited.add(start);
+//
 //        while (!stack.isEmpty()) {
-//            String vertex = stack.pop();
-//            if (!visited.contains(vertex)) {
-//                visited.add(vertex);
-//                for (Vertex v : g1.getAdjVertices(vertex)) {
-//                    stack.push(v.label);
+//            Node tempNode = (Node) stack.pop();
+//            result.add(tempNode);
+//
+//            for (Edge neighbor : (HashSet<Edge>) tempNode.neighbors) {
+//                if (!visited.contains(neighbor.node)) {
+//                    stack.push(neighbor.node);
+//                    visited.add(neighbor.node);
 //                }
 //            }
 //        }
-//        return visited;
+//        return result;
 //    }
-
 }
